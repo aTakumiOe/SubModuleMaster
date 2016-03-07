c065a8dc401ac9c2a9d2b224d0874ceec5ef29be
Takumi Oe <takumi.oe@access-company.com>
2016-03-07 17:18:42 +0900

    submodule configured.

--- app/src/main/java/com/example/submodulemaster/MainActivity.java
+++ app/src/main/java/com/example/submodulemaster/MainActivity.java
@@ -1,13 +1,1 @@
-package com.example.submodulemaster;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+package com.example.submodulemaster;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;import android.util.Log;import com.example.submodulelibrary.SubModuleConst;public class MainActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);        Log.d("*** Sub Module Test ***", SubModuleConst.SAMPLE_TEXT);    }}
